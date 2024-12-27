45020dc866c823f000a0e456e064edbb99778bfc
Jeff Loiselle <jeff@loiselles.com>
2024-12-27 17:58:58 -0600

    Update lib/utils/time.ts [BoltSync]

--- lib/utils/time.ts
+++ lib/utils/time.ts
@@ -1,4 +1,4 @@
 export function parseTimestamp(timestamp: string): number {
   const [minutes, seconds, milliseconds] = timestamp.split(/[:.]/);
-  return parseInt(minutes) * 60 + parseInt(seconds) + parseInt(milliseconds) / 1000;
+  return parseInt(minutes) * 60 + parseInt(seconds) + parseInt(milliseconds);
 }
